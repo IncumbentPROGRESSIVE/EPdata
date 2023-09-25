f3950919cec737cf0adc6b923668da7151bcd661
Colin Leary <113806491+IncumbentPROGRESSIVE@users.noreply.github.com>
2023-09-25 12:19:12 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+var toggleButton = document.getElementById('toggleButton');
+var electionIframe = document.getElementById('electionIframe');
+var caption = document.querySelector('.caption');
+var darkModeButton = document.getElementById('darkModeButton');
+var darkModeButton2 = document.getElementById('darkModeButton2');
+var darkModeButton3 = document.getElementById('darkModeButton3');
+
+
+toggleButton.addEventListener('click', function() {
+  if (electionIframe.src.includes('senate')) {
+    electionIframe.src = 'https://steepatticstairs.github.io/ElectionMap/?mode=gubernatorial';
+    toggleButton.textContent = 'Gubernatorial';
+    caption.textContent = 'Gubernatorial'; // Change the caption text
+  } else if (electionIframe.src.includes('gubernatorial')) {
+    electionIframe.src = 'https://steepatticstairs.github.io/ElectionMap/';
+    toggleButton.textContent = 'House';
+     caption.textContent = 'House of Representatives'; // Change the caption text
+  } else {
+    electionIframe.src = 'https://steepatticstairs.github.io/ElectionMap/?mode=senate';
+    toggleButton.textContent = 'Senate';
+     caption.textContent = 'Senate'; // Change the caption text
+  }
+});
+
+
+
+darkModeButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  darkModeButton.classList.toggle('on');
+
+  // Save Dark Mode setting in localStorage
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('dark-mode', 'true');
+  } else {
+    localStorage.setItem('dark-mode', 'false');
+  }
+});
+
+darkModeButton2.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  darkModeButton2.classList.toggle('on');
+
+  // Save Dark Mode setting in localStorage
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('dark-mode', 'true');
+  } else {
+    localStorage.setItem('dark-mode', 'false');
+  }
+});
+
+darkModeButton3.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  darkModeButton3.classList.toggle('on');
+
+  // Save Dark Mode setting in localStorage
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('dark-mode', 'true');
+  } else {
+    localStorage.setItem('dark-mode', 'false');
+  }
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Check for dark mode preference on page load
+  if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-mode');
+    darkModeButton.classList.add('on');
+    darkModeButton2.classList.add('on');
+    darkModeButton3.classList.add('on');
+  }
+});
